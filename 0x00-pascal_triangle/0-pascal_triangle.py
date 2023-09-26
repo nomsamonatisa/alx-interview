92b9e37ade35d808e4f3a78befccfc24046f1b37
Mamosa Monatisa <paballomonatisa7@gmail.com>
2023-09-26 09:04:15 +0000

    Pascal's Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
+
+
+def print_triangle(triangle):
+    for row in triangle:
+        print("[{}]".format(",".join(map(str, row))))
+
+
+if __name__ == "__main__":
+    print_triangle(pascal_triangle(5))
+
